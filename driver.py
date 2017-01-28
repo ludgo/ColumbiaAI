f38b11e0e322c0edeb6f892cc98716bc4980f6b0
ludgo <ludgogit@gmail.com>
2017-01-28 16:27:45 +0100

    subm1

--- driver.py
+++ driver.py
@@ -1,4 +1,4 @@
-import sys, string
+import sys, string, time
 from Queue import Queue
 from model import Board, State
 
@@ -10,18 +10,21 @@ def bfs(initState, goalSeq):
 	explored = []
 
 	while not q.empty():
+		dic['max_fringe_size'] = max(dic['max_fringe_size'], q.qsize())
 		st = q.get()
 		st.findChildren()
 		explored.append(st.board.seq)
 
 		if st.board.seq == goalSeq:
 			dic['search_depth'] = st.depth
+			dic['fringe_size'] = q.qsize()
 			return st
+
+		dic['nodes_expanded'] = dic['nodes_expanded'] + 1
 
 		for child in st.udlr:
 			if child and child.board.seq not in explored:
 				q.put(child)
-				dic['nodes_expanded'] = dic['nodes_expanded'] + 1
 				dic['max_search_depth'] = max(dic['max_search_depth'], child.depth)
 
 	return None
@@ -32,7 +35,9 @@ board = string.replace(sys.argv[2], ",", "")
 root = State( Board(board), 0 )
 
 if method == "bfs":
+	start_time = time.time()
 	bottom = bfs(root, "012345678")
+	dic['running_time'] = '%s' % (time.time() - start_time)
 
 	while bottom:
 		if bottom.move:
@@ -41,5 +46,8 @@ if method == "bfs":
 		bottom = bottom.parent
 
 	print dic
+	with open('output.txt', 'w') as f:
+		for key, value in dic.items():
+			f.write('%s: %s\n' % (key, value))
 
 

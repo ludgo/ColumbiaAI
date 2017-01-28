7c905273d1ae4ead215dd82a7088a8eaf78becb9
ludgo <ludgogit@gmail.com>
2017-01-28 13:05:01 +0100

    up

--- driver.py
+++ driver.py
@@ -1,30 +1,39 @@
-import sys, string
-
+import sys, string, copy
 
 
 def toArray(str):
 	arr = []
-	for i in xrange(3):
-		arr.append([])
-		for j in xrange(3):
-			arr[i].append(str[3*i+j])
+	for i in xrange(9):
+		arr.append(str[i])
 	return arr
+
+def swap(array, indexA, indexB):
+	c = []
+	c = copy.copy(array)
+	c[indexA] = array[indexB]
+	c[indexB] = array[indexA]
+	print array
+	print c
+	return c
 
 class Board(object):
 
 	def __init__(self, board):
 		nocommas = string.replace(board, ",", "")
 		self.blank = string.index(nocommas, "0")
-		print self.blank
 		self.array = toArray(nocommas)
-		print self.array
 
+	def up(self):
+		pos = self.blank - 3
+		if pos in range(9):
+			return swap(self.array, pos, self.blank)
+		return None
 
 method = sys.argv[1]
 board = sys.argv[2]
 
 b = Board(board)
-
+b.up()
 
 
 
